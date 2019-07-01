f78903b51f712094b540d6c98686b845bc70e29f
Toru Takatori <i@218.jp>
2019-07-01 16:36:53 +0900

    [add]主要クラスアップ

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cards msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cards from './components/Cards.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cards
   }
 }
 </script>
---
+++ src/components/Cards.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script>
+
+import Iterator from '@/assets/js/libs/tk90755/utils/iterator/Iterator.js'
+import {TweenMax} from "gsap";
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted(){
+
+  },
+  methods: {
+    update(delta){
+      // if(this.mixer !== undefined)this.mixer.update(delta);
+    },
+    myCharacter(){
+      // return this.character;
+    },
+    myMixer(){
+      // return this.mixer;
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
