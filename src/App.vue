af574edcdc09a6f82219f8d881aeff8a7319431b
Toru Takatori <i@218.jp>
2019-07-03 12:17:27 +0900

    [update]run_prettify.jsの記述追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <template>
   <div id="app">
     <Cards methodName='next'/>
+    <pre class="prettyprint lang-js">
+      <code>
+      let item = (this.iterator.hasNext())?this.iterator.next():this.iterator.fast();
+      item.content.excute()
+      </code>
+    </pre>
     <Cards methodName='prev'/>
+    <pre class="prettyprint lang-js">
+      <code>
+      let item = (this.iterator.hasPrev())?this.iterator.prev():this.iterator.last();
+      item.content.excute()
+      </code>
+    </pre>
     <Cards methodName='shuffle'/>
+    <pre class="prettyprint lang-js">
+      <code>
+      let init =()=>{
+        console.log('shuffle init')
+      }
+      let update =(item)=>{
+        console.log('shuffle update' + item.name)
+      }
+      let complete =()=>{
+        console.log('shuffle complete')
+      }
+      let item = this.iterator.shuffle(init, update, complete);
+      item.content.excute()
+      </code>
+    </pre>
     <Cards methodName='random'/>
+    <pre class="prettyprint lang-js">
+      <code>
+      let item = this.iterator.random();
+      item.content.excute()
+      </code>
+    </pre>
   </div>
 </template>
 
